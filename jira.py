b2cb21d2f8453b955c67ad027e846a72458e5cc6
Wenbao Xu <mail@xuwenbao.me>
2016-11-30 16:54:20 +0800

    add config demo

--- jira.py
+++ jira.py
@@ -4,9 +4,15 @@ from errbot import BotPlugin, botcmd
 class Jira(BotPlugin):
     """Jira plugin for Errbot"""
 
+    def get_configuration_template(self):
+        return {
+            'ID_TOKEN': '00112233445566778899aabbccddeeff',
+            'USERNAME':'changeme'
+        }
+
     def callback_mention(self, message, mentioned_people):
         if self.bot_identifier in mentioned_people:
-            self.send(message.frm, u'找我嘎哈？')
+            self.send(message.frm, '找我嘎哈？')
 
     @botcmd
     def jira_add(self, mess, args):
